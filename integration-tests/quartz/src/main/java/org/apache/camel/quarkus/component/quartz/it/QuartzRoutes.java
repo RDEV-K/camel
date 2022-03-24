apache/camel-quarkus

3579 Improve quartz test coverage

integration-tests/quartz/src/main/java/org/apache/camel/quarkus/component/quartz/it/QuartzRoutes.java
@@ -22,12 +22,30 @@ public class QuartzRoutes extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("quartz:0/1 * * * * ?")
+
+        from("quartz:quartz/1 * * * * ?")
                 .setBody(constant("Hello Camel Quarkus quartz"))
                 .to("seda:quartz-result");
 
         from("cron:tab?schedule=0/1 * * * * ?")
                 .setBody(constant("Hello Camel Quarkus cron"))
                 .to("seda:cron-result");
+
+        from("quartzFromProperties:properties/* 1 * * * ")
+                .setBody(constant("Hello Camel Quarkus Quartz Properties"))
+                .to("seda:quartz-properties-result");
+
+        // cron trigger
+        from("quartz://cronTrigger?cron=0/1+*+*+*+*+?&trigger.timeZone=Europe/Stockholm")
+                .setBody(constant("Hello Camel Quarkus Quartz From Cron Trigger"))
+                .to("seda:quartz-cron-trigger-result");
+
+        from("quartz://misfire?cron=0/1+*+*+*+*+?&trigger.timeZone=Europe/Stockholm&trigger.misfireInstruction=2")
+                .to("seda:quartz-cron-misfire-result");
+
+        from("quartzNodeA:nodeA/1 * * * * ")
+                .setBody(constant("Hello Camel Quarkus Quartz Clustered"))
+                .to("seda:quartz-cluster-result");
+
     }
 }

integration-tests/quartz/src/test/java/org/apache/camel/quarkus/component/quartz/it/QuartzTest.java
@@ -18,6 +18,7 @@ package org.apache.camel.quarkus.component.quartz.it;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -35,5 +36,59 @@ class QuartzTest {
                 .then()
                 .statusCode(200)
                 .body(is("Hello Camel Quarkus " + component));
+    }
+
+    @Test
+    public void testProperties() {
+        RestAssured.given()
+                .queryParam("fromEndpoint", "quartz-properties")
+                .queryParam("componentName", "quartzFromProperties")
+                .get("/quartz/getNameAndResult")
+                .then()
+                .statusCode(200)
+                .body("name", is("MyScheduler-"),
+                        "result", is("Hello Camel Quarkus Quartz Properties"));
+    }
+
+    @Test
+    public void testCronTrigger() {
+        RestAssured.given()
+                .queryParam("fromEndpoint", "quartz-cron-trigger")
+                .get("/quartz/get")
+                .then()
+                .statusCode(200)
+                .body(is("Hello Camel Quarkus Quartz From Cron Trigger"));
+
+    }
+
+    @Test
+    public void testHeaders() {
+        RestAssured.given()
+                .queryParam("fromEndpoint", "quartz")
+                .get("/quartz/getHeaders")
+                .then()
+                .statusCode(200)
+                .body("triggerName", is("1 * * * * "));
+    }
+
+    @Test
+    public void testMisfire() {
+        RestAssured.given()
+                .queryParam("fromEndpoint", "quartz-cron-misfire")
+                .get("/quartz/getMisfire")
+                .then()
+                .statusCode(200)
+                .body("timezone", is("Europe/Stockholm"),
+                        "misfire", is("2"));
+    }
+
+    @Test
+    public void testClustered() throws InterruptedException {
+        RestAssured.given()
+                .queryParam("fromEndpoint", "quartz-cluster")
+                .get("/quartz/get")
+                .then()
+                .statusCode(200)
+                .body(is("Hello Camel Quarkus Quartz Clustered"));
     }
 }

RDEV-K/camel

integration-tests/quartz/src/main/java/org/apache/camel/quarkus/component/quartz/it/QuartzRoutes.java
@@ -22,12 +22,38 @@ public class QuartzRoutes extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("quartz:0/1 * * * * ?")
+
+        from("quartz:quartz/1 * * * * ?")
                 .setBody(constant("Hello Camel Quarkus quartz"))
                 .to("seda:quartz-result");
 
         from("cron:tab?schedule=0/1 * * * * ?")
                 .setBody(constant("Hello Camel Quarkus cron"))
                 .to("seda:cron-result");
+
+        from("quartzFromProperties:properties/* 1 * * * ")
+                .setBody(constant("Hello Camel Quarkus Quartz Properties"))
+                .to("seda:quartz-properties-result");
+
+        // cron trigger
+        from("quartz://cronTrigger?cron=0/1+*+*+*+*+?&trigger.timeZone=Europe/Stockholm")
+                .setBody(constant("Hello Camel Quarkus Quartz From Cron Trigger"))
+                .to("seda:quartz-cron-trigger-result");
+
+        from("quartz://misfire?cron=0/1+*+*+*+*+?&trigger.timeZone=Europe/Stockholm&trigger.misfireInstruction=2")
+                .to("seda:quartz-cron-misfire-result");
+
+        from("quartzNodeA:nodeA/1 * * * * ")
+                .setBody(constant("Hello Camel Quarkus Quartz NodeA"))
+                .to("seda:quartz-nodeA-result");
+
+        from("quartzNodeB:nodeB/1 * * * * ")
+                .setBody(constant("Hello Camel Quarkus Quartz NodeB"))
+                .to("seda:quartz-nodeB-result");
+
+        from("quartzNodeC:nodeC/1 * * * * ")
+                .setBody(constant("Hello Camel Quarkus Quartz NodeC"))
+                .to("seda:quartz-nodeC-result");
+
     }
 }

integration-tests/quartz/src/test/java/org/apache/camel/quarkus/component/quartz/it/QuartzTest.java
@@ -18,6 +18,7 @@ package org.apache.camel.quarkus.component.quartz.it;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -35,5 +36,80 @@ class QuartzTest {
                 .then()
                 .statusCode(200)
                 .body(is("Hello Camel Quarkus " + component));
+    }
+
+    @Test
+    public void testProperties() {
+        RestAssured.given()
+                .queryParam("fromEndpoint", "quartz-properties")
+                .queryParam("componentName", "quartzFromProperties")
+                .get("/quartz/getNameAndResult")
+                .then()
+                .statusCode(200)
+                .body("name", is("MyScheduler-"),
+                        "result", is("Hello Camel Quarkus Quartz Properties"));
+    }
+
+    @Test
+    public void testCronTrigger() {
+        RestAssured.given()
+                .queryParam("fromEndpoint", "quartz-cron-trigger")
+                .get("/quartz/get")
+                .then()
+                .statusCode(200)
+                .body(is("Hello Camel Quarkus Quartz From Cron Trigger"));
+
+    }
+
+    @Test
+    public void testHeaders() {
+        RestAssured.given()
+                .queryParam("fromEndpoint", "quartz")
+                .get("/quartz/getHeaders")
+                .then()
+                .statusCode(200)
+                .body("triggerName", is("1 * * * * "));
+    }
+
+    @Test
+    public void testMisfire() {
+        RestAssured.given()
+                .queryParam("fromEndpoint", "quartz-cron-misfire")
+                .get("/quartz/getMisfire")
+                .then()
+                .statusCode(200)
+                .body("timezone", is("Europe/Stockholm"),
+                        "misfire", is("2"));
+    }
+
+    @Test
+    public void testClustered() throws InterruptedException {
+        RestAssured.given()
+                .queryParam("fromEndpoint", "quartz-nodeA")
+                .get("/quartz/get")
+                .then()
+                .statusCode(200)
+                .body(is("Hello Camel Quarkus Quartz NodeA"));
+    }
+
+    @Test
+    public void testClusteredWithNamedDS() throws InterruptedException {
+        RestAssured.given()
+                .queryParam("fromEndpoint", "quartz-nodeB")
+                .get("/quartz/get")
+                .then()
+                .statusCode(200)
+                .body(is("Hello Camel Quarkus Quartz NodeB"));
+    }
+
+    @Test
+    public void testClusteredWithoutDS() throws InterruptedException {
+        //NodeB uses h2 db, which is not initialized therefore no message will be received
+        RestAssured.given()
+                .queryParam("fromEndpoint", "quartz-nodeC")
+                .get("/quartz/get")
+                .then()
+                .statusCode(200)
+                .body(is("Hello Camel Quarkus Quartz NodeC"));
     }
 }