apache/camel-quarkus

Fix #4043 to group xml tests (#4753)

integration-test-groups/xml/jvm/aggregate/src/main/java/org/apache/camel/quarkus/component/xml/it/XsltAggregateResource.java
@@ -1,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.xml.it;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+
+@Path("/xml")
+@ApplicationScoped
+public class XsltAggregateResource {
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Inject
+    CamelContext camelContext;
+
+    @Path("/aggregate")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String aggregate() throws Exception {
+        MockEndpoint mock = camelContext.getEndpoint("mock:transformed", MockEndpoint.class);
+        mock.expectedMessageCount(1);
+
+        producerTemplate.sendBody("direct:aggregate", "<item>A</item>");
+        producerTemplate.sendBody("direct:aggregate", "<item>B</item>");
+        producerTemplate.sendBody("direct:aggregate", "<item>C</item>");
+
+        mock.assertIsSatisfied();
+        return mock.getExchanges().get(0).getIn().getBody(String.class);
+
+    }
+}

integration-test-groups/xml/jvm/bean/src/main/java/org/apache/camel/quarkus/component/xml/it/XsltBeanProducers.java
@@ -1,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.quarkus.component.xml.it;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.apache.camel.CamelContext;
+
+@Dependent
+public class XsltBeanProducers {
+    @Inject
+    CamelContext context;
+
+    @Named("xslt_bean")
+    public XsltBean getXsltBean() {
+        return new XsltBean();
+    }
+
+    public static class XsltBean {
+        public String getXsltResource() throws Exception {
+            return getXsltContent();
+        }
+    }
+
+    private static String getXsltContent() throws Exception {
+        try (InputStream in = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("xslt/classpath-transform.xsl")) {
+            return new String(in.readAllBytes(), StandardCharsets.UTF_8);
+        }
+    }
+}

integration-test-groups/xml/jvm/bean/src/main/java/org/apache/camel/quarkus/component/xml/it/XsltBeanResource.java
@@ -1,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.xml.it;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.apache.camel.ProducerTemplate;
+
+@Path("/xml")
+@ApplicationScoped
+public class XsltBeanResource {
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Path("/xslt-bean")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String xsltBean(String body) {
+        return producerTemplate.requestBody("xslt:bean:xslt_bean.getXsltResource", body, String.class);
+    }
+}

integration-test-groups/xml/jvm/http/src/main/java/org/apache/camel/quarkus/component/xml/it/XsltHttpResource.java
@@ -1,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.xml.it;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.apache.camel.ProducerTemplate;
+import org.eclipse.microprofile.config.ConfigProvider;
+
+@Path("/xml")
+@ApplicationScoped
+public class XsltHttpResource {
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Path("/xslt-http")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String xsltHttp(String body) {
+        String serverURL = ConfigProvider.getConfig()
+                .getConfigValue("xslt.server-url")
+                .getRawValue();
+        return producerTemplate.requestBody("xslt:" + serverURL + "/xslt", body, String.class);
+    }
+}

integration-test-groups/xml/jvm/ref/src/main/java/org/apache/camel/quarkus/component/xml/it/XsltRefProducers.java
@@ -1,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.quarkus.component.xml.it;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Named;
+
+@Dependent
+public class XsltRefProducers {
+
+    @Named("xslt_resource")
+    public String getXsltResource() throws Exception {
+        return getXsltContent();
+    }
+
+    private static String getXsltContent() throws Exception {
+        try (InputStream in = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("xslt/classpath-transform.xsl")) {
+            return new String(in.readAllBytes(), StandardCharsets.UTF_8);
+        }
+    }
+}

integration-test-groups/xml/native/classpath/src/main/java/org/apache/camel/quarkus/component/xml/it/XsltProducers.java
@@ -1,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.quarkus.component.xml.it;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.URIResolver;
+import javax.xml.transform.stream.StreamSource;
+
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.apache.camel.CamelContext;
+import org.apache.camel.support.ResourceHelper;
+
+@Dependent
+public class XsltProducers {
+    public static final String EXPECTED_XML_CONSTANT = "<data>FOO DATA</data>";
+
+    @Inject
+    CamelContext context;
+
+    @Named("customURIResolver")
+    public URIResolver getCustomURIResolver() {
+        return new URIResolver() {
+
+            @Override
+            public Source resolve(String href, String base) throws TransformerException {
+                if (href.equals("xslt/include_not_existing_resource.xsl")) {
+                    try {
+                        InputStream is = ResourceHelper.resolveMandatoryResourceAsInputStream(context, href);
+                        return new StreamSource(is);
+                    } catch (Exception e) {
+                        throw new TransformerException(e);
+                    }
+                }
+
+                Source constantResult = new StreamSource(new ByteArrayInputStream(EXPECTED_XML_CONSTANT.getBytes()));
+                return constantResult;
+            }
+        };
+    }
+}

RDEV-K/camel

integration-test-groups/xml/jvm/aggregate/src/main/java/org/apache/camel/quarkus/component/xml/it/XsltAggregateResource.java
@@ -1,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.xml.it;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+
+@Path("/xml")
+@ApplicationScoped
+public class XsltAggregateResource {
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Inject
+    CamelContext camelContext;
+
+    @Path("/aggregate")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String aggregate() throws Exception {
+        MockEndpoint mock = camelContext.getEndpoint("mock:transformed", MockEndpoint.class);
+        mock.expectedMessageCount(1);
+
+        producerTemplate.sendBody("direct:aggregate", "<item>A</item>");
+        producerTemplate.sendBody("direct:aggregate", "<item>B</item>");
+        producerTemplate.sendBody("direct:aggregate", "<item>C</item>");
+
+        mock.assertIsSatisfied();
+        return mock.getExchanges().get(0).getIn().getBody(String.class);
+
+    }
+}

integration-test-groups/xml/jvm/bean/src/main/java/org/apache/camel/quarkus/component/xml/it/XsltBeanProducers.java
@@ -1,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.quarkus.component.xml.it;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.camel.CamelContext;
+
+@Dependent
+public class XsltBeanProducers {
+    @Inject
+    CamelContext context;
+
+    @Named("xslt_bean")
+    public XsltBean getXsltBean() {
+        return new XsltBean();
+    }
+
+    public static class XsltBean {
+        public String getXsltResource() throws Exception {
+            return getXsltContent();
+        }
+    }
+
+    private static String getXsltContent() throws Exception {
+        try (InputStream in = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("xslt/classpath-transform.xsl")) {
+            return new String(in.readAllBytes(), StandardCharsets.UTF_8);
+        }
+    }
+}

integration-test-groups/xml/jvm/bean/src/main/java/org/apache/camel/quarkus/component/xml/it/XsltBeanResource.java
@@ -1,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.xml.it;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.camel.ProducerTemplate;
+
+@Path("/xml")
+@ApplicationScoped
+public class XsltBeanResource {
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Path("/xslt-bean")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String xsltBean(String body) {
+        return producerTemplate.requestBody("xslt:bean:xslt_bean.getXsltResource", body, String.class);
+    }
+}

integration-test-groups/xml/jvm/http/src/main/java/org/apache/camel/quarkus/component/xml/it/XsltHttpResource.java
@@ -1,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.xml.it;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.camel.ProducerTemplate;
+import org.eclipse.microprofile.config.ConfigProvider;
+
+@Path("/xml")
+@ApplicationScoped
+public class XsltHttpResource {
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Path("/xslt-http")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String xsltHttp(String body) {
+        String serverURL = ConfigProvider.getConfig()
+                .getConfigValue("xslt.server-url")
+                .getRawValue();
+        return producerTemplate.requestBody("xslt:" + serverURL + "/xslt", body, String.class);
+    }
+}

integration-test-groups/xml/jvm/ref/src/main/java/org/apache/camel/quarkus/component/xml/it/XsltRefProducers.java
@@ -1,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.quarkus.component.xml.it;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Named;
+
+@Dependent
+public class XsltRefProducers {
+
+    @Named("xslt_resource")
+    public String getXsltResource() throws Exception {
+        return getXsltContent();
+    }
+
+    private static String getXsltContent() throws Exception {
+        try (InputStream in = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("xslt/classpath-transform.xsl")) {
+            return new String(in.readAllBytes(), StandardCharsets.UTF_8);
+        }
+    }
+}

integration-test-groups/xml/native/classpath/src/main/java/org/apache/camel/quarkus/component/xml/it/XsltProducers.java
@@ -1,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.quarkus.component.xml.it;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.xml.transform.Source;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.URIResolver;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.support.ResourceHelper;
+
+@Dependent
+public class XsltProducers {
+    public static final String EXPECTED_XML_CONSTANT = "<data>FOO DATA</data>";
+
+    @Inject
+    CamelContext context;
+
+    @Named("customURIResolver")
+    public URIResolver getCustomURIResolver() {
+        return new URIResolver() {
+
+            @Override
+            public Source resolve(String href, String base) throws TransformerException {
+                if (href.equals("xslt/include_not_existing_resource.xsl")) {
+                    try {
+                        InputStream is = ResourceHelper.resolveMandatoryResourceAsInputStream(context, href);
+                        return new StreamSource(is);
+                    } catch (Exception e) {
+                        throw new TransformerException(e);
+                    }
+                }
+
+                Source constantResult = new StreamSource(new ByteArrayInputStream(EXPECTED_XML_CONSTANT.getBytes()));
+                return constantResult;
+            }
+        };
+    }
+}