apache/camel-quarkus

Snmp: Extend test coverage #4797

integration-tests-jvm/snmp/src/main/java/org/apache/camel/quarkus/component/snmp/it/SnmpResource.java
@@ -16,35 +16,129 @@
  */
 package org.apache.camel.quarkus.component.snmp.it;
 
+import java.util.Deque;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.inject.Named;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.apache.camel.CamelContext;
-import org.jboss.logging.Logger;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.snmp.SnmpMessage;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.snmp4j.PDU;
+import org.snmp4j.PDUv1;
+import org.snmp4j.mp.SnmpConstants;
+import org.snmp4j.smi.OID;
+import org.snmp4j.smi.OctetString;
+import org.snmp4j.smi.TimeTicks;
+import org.snmp4j.smi.Variable;
+import org.snmp4j.smi.VariableBinding;
 
 @Path("/snmp")
 @ApplicationScoped
 public class SnmpResource {
 
-    private static final Logger LOG = Logger.getLogger(SnmpResource.class);
+    @ConfigProperty(name = "snmpListenAddress")
+    String snmpListenAddress;
 
-    private static final String COMPONENT_SNMP = "snmp";
     @Inject
-    CamelContext context;
+    @Named("snmpTrapResults")
+    Map<String, Deque<SnmpMessage>> snmpResults;
 
-    @Path("/load/component/snmp")
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Path("/producePDU")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public Response loadComponentSnmp() throws Exception {
-        /* This is an autogenerated test */
-        if (context.getComponent(COMPONENT_SNMP) != null) {
-            return Response.ok().build();
-        }
-        LOG.warnf("Could not load [%s] from the Camel context", COMPONENT_SNMP);
-        return Response.status(500, COMPONENT_SNMP + " could not be loaded from the Camel context").build();
+    public Response producePDU() {
+        String url = String.format("snmp://%s?retries=1", snmpListenAddress);
+        SnmpMessage pdu = producerTemplate.requestBody(url, "", SnmpMessage.class);
+
+        String response = pdu.getSnmpMessage().getVariableBindings().stream()
+                .filter(vb -> vb.getOid().equals(SnmpConstants.sysDescr))
+                .map(vb -> vb.getVariable().toString())
+                .collect(Collectors.joining());
+
+        return Response.ok(response).build();
+    }
+
+    @Path("/sendPoll")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response sendPoll() {
+        SnmpMessage pdu = producerTemplate.requestBody("direct:producePoll", "", SnmpMessage.class);
+
+        String response = pdu.getSnmpMessage().getVariableBindings().stream()
+                .filter(vb -> vb.getOid().equals(SnmpConstants.sysDescr))
+                .map(vb -> vb.getVariable().toString())
+                .collect(Collectors.joining());
+
+        return Response.ok(response).build();
+    }
+
+    @Path("/getNext")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getNext(String payload) {
+        String url = String.format("snmp://%s?type=GET_NEXT&retries=1&protocol=udp&oids=%s", snmpListenAddress,
+                SnmpConstants.sysDescr);
+        List<SnmpMessage> pdu = producerTemplate.requestBody(url, "", List.class);
+
+        String response = pdu.stream()
+                .flatMap(m -> m.getSnmpMessage().getVariableBindings().stream())
+                .filter(vb -> vb.getOid().equals(SnmpConstants.sysDescr))
+                .map(vb -> vb.getVariable().toString())
+                .collect(Collectors.joining(","));
+
+        return Response.ok(response).build();
+    }
+
+    @Path("/produceTrap")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response sendTrap(String payload) {
+        String url = "snmp:127.0.0.1:1662?protocol=udp&type=TRAP&snmpVersion=0)";
+        PDU trap = createTrap(payload);
+
+        producerTemplate.sendBody(url, trap);
+
+        return Response.ok().build();
+    }
+
+    @Path("/results")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response results(String from) throws Exception {
+        OID oid = "trap".equals(from) ? new OID("1.2.3.4.5") : SnmpConstants.sysDescr;
+        String result = snmpResults.get(from).stream().map(m -> m.getSnmpMessage().getVariable(oid).toString())
+                .collect(Collectors.joining(","));
+
+        return Response.ok(result).build();
+    }
+
+    public PDU createTrap(String payload) {
+        PDUv1 trap = new PDUv1();
+        trap.setGenericTrap(PDUv1.ENTERPRISE_SPECIFIC);
+        trap.setSpecificTrap(1);
+
+        OID oid = new OID("1.2.3.4.5");
+        trap.add(new VariableBinding(SnmpConstants.snmpTrapOID, oid));
+        trap.add(new VariableBinding(SnmpConstants.sysUpTime, new TimeTicks(5000))); // put your uptime here
+        trap.add(new VariableBinding(SnmpConstants.sysDescr, new OctetString("System Description")));
+        trap.setEnterprise(oid);
+
+        //Add Payload
+        Variable var = new OctetString(payload);
+        trap.add(new VariableBinding(oid, var));
+        return trap;
     }
 }

integration-tests-jvm/snmp/src/main/java/org/apache/camel/quarkus/component/snmp/it/SnmpRoute.java
@@ -1,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.snmp.it;
+
+import java.util.Deque;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.snmp.SnmpMessage;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ApplicationScoped
+public class SnmpRoute extends RouteBuilder {
+
+    @ConfigProperty(name = "snmpListenAddress")
+    String snmpListenAddress;
+
+    @Inject
+    @Named("snmpTrapResults")
+    Map<String, Deque<SnmpMessage>> snmpResults;
+
+    @Override
+    public void configure() {
+        //TRAP consumer
+        from("snmp:0.0.0.0:1662?protocol=udp&type=TRAP&snmpVersion=0")
+                .process(e -> snmpResults.get("trap").add(e.getIn().getBody(SnmpMessage.class)));
+
+        //POLL consumer
+        from("snmp://" + snmpListenAddress + "?protocol=udp&type=POLL&snmpVersion=0&oids=1.3.6.1.2.1.1.5.0")
+                .process(e -> snmpResults.get("poll").add(e.getIn().getBody(SnmpMessage.class)));
+    }
+
+    static class Producers {
+        @jakarta.enterprise.inject.Produces
+        @Singleton
+        @Named("snmpTrapResults")
+        Map<String, Deque<SnmpMessage>> snmpResults() {
+            Map<String, Deque<SnmpMessage>> map = new ConcurrentHashMap<>();
+            map.put("trap", new ConcurrentLinkedDeque());
+            map.put("poll", new ConcurrentLinkedDeque());
+            return map;
+        }
+    }
+}

RDEV-K/camel

integration-tests-jvm/snmp/src/main/java/org/apache/camel/quarkus/component/snmp/it/SnmpResource.java
@@ -16,36 +16,130 @@
  */
 package org.apache.camel.quarkus.component.snmp.it;
 
+import java.util.Deque;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.apache.camel.CamelContext;
-import org.jboss.logging.Logger;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.snmp.SnmpMessage;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.snmp4j.PDU;
+import org.snmp4j.PDUv1;
+import org.snmp4j.mp.SnmpConstants;
+import org.snmp4j.smi.OID;
+import org.snmp4j.smi.OctetString;
+import org.snmp4j.smi.TimeTicks;
+import org.snmp4j.smi.Variable;
+import org.snmp4j.smi.VariableBinding;
 
 @Path("/snmp")
 @ApplicationScoped
 public class SnmpResource {
 
-    private static final Logger LOG = Logger.getLogger(SnmpResource.class);
+    @ConfigProperty(name = "snmpListenAddress")
+    String snmpListenAddress;
 
-    private static final String COMPONENT_SNMP = "snmp";
     @Inject
-    CamelContext context;
+    @Named("snmpTrapResults")
+    Map<String, Deque<SnmpMessage>> snmpResults;
 
-    @Path("/load/component/snmp")
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Path("/producePDU")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public Response loadComponentSnmp() throws Exception {
-        /* This is an autogenerated test */
-        if (context.getComponent(COMPONENT_SNMP) != null) {
-            return Response.ok().build();
-        }
-        LOG.warnf("Could not load [%s] from the Camel context", COMPONENT_SNMP);
-        return Response.status(500, COMPONENT_SNMP + " could not be loaded from the Camel context").build();
+    public Response producePDU() {
+        String url = String.format("snmp://%s?retries=1", snmpListenAddress);
+        SnmpMessage pdu = producerTemplate.requestBody(url, "", SnmpMessage.class);
+
+        String response = pdu.getSnmpMessage().getVariableBindings().stream()
+                .filter(vb -> vb.getOid().equals(SnmpConstants.sysDescr))
+                .map(vb -> vb.getVariable().toString())
+                .collect(Collectors.joining());
+
+        return Response.ok(response).build();
+    }
+
+    @Path("/sendPoll")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response sendPoll() {
+        SnmpMessage pdu = producerTemplate.requestBody("direct:producePoll", "", SnmpMessage.class);
+
+        String response = pdu.getSnmpMessage().getVariableBindings().stream()
+                .filter(vb -> vb.getOid().equals(SnmpConstants.sysDescr))
+                .map(vb -> vb.getVariable().toString())
+                .collect(Collectors.joining());
+
+        return Response.ok(response).build();
+    }
+
+    @Path("/getNext")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getNext(String payload) {
+        String url = String.format("snmp://%s?type=GET_NEXT&retries=1&protocol=udp&oids=%s", snmpListenAddress,
+                SnmpConstants.sysDescr);
+        List<SnmpMessage> pdu = producerTemplate.requestBody(url, "", List.class);
+
+        String response = pdu.stream()
+                .flatMap(m -> m.getSnmpMessage().getVariableBindings().stream())
+                .filter(vb -> vb.getOid().equals(SnmpConstants.sysDescr))
+                .map(vb -> vb.getVariable().toString())
+                .collect(Collectors.joining(","));
+
+        return Response.ok(response).build();
+    }
+
+    @Path("/produceTrap")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response sendTrap(String payload) {
+        String url = "snmp:127.0.0.1:1662?protocol=udp&type=TRAP&snmpVersion=0)";
+        PDU trap = createTrap(payload);
+
+        producerTemplate.sendBody(url, trap);
+
+        return Response.ok().build();
+    }
+
+    @Path("/results")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response results(String from) throws Exception {
+        OID oid = "trap".equals(from) ? new OID("1.2.3.4.5") : SnmpConstants.sysDescr;
+        String result = snmpResults.get(from).stream().map(m -> m.getSnmpMessage().getVariable(oid).toString())
+                .collect(Collectors.joining(","));
+
+        return Response.ok(result).build();
+    }
+
+    public PDU createTrap(String payload) {
+        PDUv1 trap = new PDUv1();
+        trap.setGenericTrap(PDUv1.ENTERPRISE_SPECIFIC);
+        trap.setSpecificTrap(1);
+
+        OID oid = new OID("1.2.3.4.5");
+        trap.add(new VariableBinding(SnmpConstants.snmpTrapOID, oid));
+        trap.add(new VariableBinding(SnmpConstants.sysUpTime, new TimeTicks(5000))); // put your uptime here
+        trap.add(new VariableBinding(SnmpConstants.sysDescr, new OctetString("System Description")));
+        trap.setEnterprise(oid);
+
+        //Add Payload
+        Variable var = new OctetString(payload);
+        trap.add(new VariableBinding(oid, var));
+        return trap;
     }
 }

integration-tests-jvm/snmp/src/main/java/org/apache/camel/quarkus/component/snmp/it/SnmpRoute.java
@@ -1,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.snmp.it;
+
+import java.util.Deque;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import javax.ws.rs.Produces;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.snmp.SnmpMessage;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ApplicationScoped
+public class SnmpRoute extends RouteBuilder {
+
+    @ConfigProperty(name = "snmpListenAddress")
+    String snmpListenAddress;
+
+    @Inject
+    @Named("snmpTrapResults")
+    Map<String, Deque<SnmpMessage>> snmpResults;
+
+    @Override
+    public void configure() {
+        //TRAP consumer
+        from("snmp:0.0.0.0:1662?protocol=udp&type=TRAP&snmpVersion=0")
+                .process(e -> snmpResults.get("trap").add(e.getIn().getBody(SnmpMessage.class)));
+
+        //POLL consumer
+        from("snmp://" + snmpListenAddress + "?protocol=udp&type=POLL&snmpVersion=0&oids=1.3.6.1.2.1.1.5.0")
+                .process(e -> snmpResults.get("poll").add(e.getIn().getBody(SnmpMessage.class)));
+    }
+
+    static class Producers {
+        @Produces
+        @Singleton
+        @Named("snmpTrapResults")
+        Map<String, Deque<SnmpMessage>> snmpResults() {
+            Map<String, Deque<SnmpMessage>> map = new ConcurrentHashMap<>();
+            map.put("trap", new ConcurrentLinkedDeque());
+            map.put("poll", new ConcurrentLinkedDeque());
+            return map;
+        }
+    }
+}